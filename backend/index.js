f63d71e772457740e7efea1f0577303ba9c4d90f
Chris Koehne <cdkoehne@gmail.com>
2021-01-22 22:17:58 -0500

    intial database setup with demo functions

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 
 console.log("\x1b[36m%s\x1b[0m", "Starting BACKEND...");
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
 
 
-const defaultURI = 'mongodb+srv://cappy:53VqF3Fqc7HMuJty@cmap-cluster.jqzmq.mongodb.net/initial?retryWrites=true&w=majority';
+const defaultURI = 'mongodb+srv://cappy:cappy@cmap-cluster.jqzmq.mongodb.net/Reviews?retryWrites=true&w=majority';
 
 mongoose
   .connect(
@@ -25,13 +25,20 @@ db.once('open', () => {
   console.log("MongoDB Connected…");
 });
 
+const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 })
+
+app.use(require(path.resolve(__dirname, "./Routers/router")));
+
+
 
 const PORT = process.env.PORT || 5000;
 
