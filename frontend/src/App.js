02eed2b3b996c8197f2ad0c69576cce04ca3c5ed
Chris Koehne <cdkoehne@gmail.com>
2021-01-23 15:34:07 -0500

    added tom's stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/* import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Map from './components/pages/map';
@@ -49,27 +49,34 @@ export default function App() {
       ></GoogleMap>
     </div>
   );
+}*/
+
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Map from "./components/pages/map";
+import Review from "./components/pages/review";
+import NavBar from "./components/navbar";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  async componentDidMount() {}
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <p>visible on all pages</p>
+        <BrowserRouter>
+          <Route path={["/", "/review", "/map"]} exact component={NavBar}/>
+          <Route path="/review" exact component={Review} />
+          <Route path="/map" exact component={Map} />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   async componentDidMount() {}
-
-//   render() {
-//     return (
-//       <div className='container-fluid'>
-//         <p>visible on all pages</p>
-//         <BrowserRouter>
-//           <Route path={['/', '/review', '/map']} exact component={NavBar} />
-//           <Route path='/review' exact component={Review} />
-//           <Route path='/map' exact component={Map} />
-//         </BrowserRouter>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
+export default App;
