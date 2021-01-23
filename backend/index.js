cf3dfb35ca0dc13f73f87bc9d3ae47b6f8d9c11d
Chris Koehne <cdkoehne@gmail.com>
2021-01-22 21:28:36 -0500

    connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,29 @@
+
+console.log("\x1b[36m%s\x1b[0m", "Starting BACKEND...");
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
-const apiPort = 3000
+
+
+const defaultURI = 'mongodb+srv://cappy:53VqF3Fqc7HMuJty@cmap-cluster.jqzmq.mongodb.net/initial?retryWrites=true&w=majority';
+
+mongoose
+  .connect(
+    defaultURI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("MongoDB Connected…");
+});
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -12,4 +33,12 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+const PORT = process.env.PORT || 5000;
+
+/**
+ * Finally, after we have finished adding all the endpoints we want, we tell the app to listen on the desired port.
+ */
+app.listen(PORT, () => {
+  console.log("\x1b[36m%s\x1b[0m", "Startup Successful, BACKEND is ONLINE");
+  console.log(`BACKEND listening on port ${PORT}...`);
+});
