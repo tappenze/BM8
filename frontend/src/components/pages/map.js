19fe900fe36e99abf3ec4139ba9d1da5fc40ea50
Tom Appenzeller <tappenze@purdue.edu>
2021-01-22 22:29:37 -0500

    Prep for frontend

---
+++ frontend/src/components/pages/map.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Form } from "react-bootstrap";
+import "./style.css";
+
+export default class Map extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount() {
+
+    }
+
+    render() {
+        return(<div><p>The map page</p></div>);
+    }
+}
